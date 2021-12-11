7e4ab70d37a6024d5ee09e1c782ed287b59bf4cc
Mohand Abdo <mohand30466@gmail.com>
2021-12-11 09:24:19 +0200

    add log out buttom

--- src/compenent/Userprofile/Userprofile.js
+++ src/compenent/Userprofile/Userprofile.js
@@ -1,12 +1,22 @@
 import { React } from 'react'
+import { useCookies } from 'react-cookie';
 
 
 export default function Userprofile() {
+    const [token,setToken, removetoken]=useCookies()
+
+
+    const logoutclick =()=>{
+        console.log("log out clicked");
+        removetoken(['mykey'])
+        window.location.href="/"
+    }
 
 
 
     return ( <
         div >
+        <button className='logout' onClick={logoutclick}>log out</button>
         <
         h1 >
         Welcome to user profile page <
